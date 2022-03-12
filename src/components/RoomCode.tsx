b4db1ce5d84943872e7a9bc90316cd63b73201c7
assiscs2 <50466377+assiscs2@users.noreply.github.com>
2022-03-11 21:59:51 -0300

    first commit

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -15,7 +15,7 @@ export function RoomCode(props: RoomCodeProps) {
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #-Mr4sZdksBkgLcUVLTvJ</span>
+      <span>Sala {props.code}</span>
     </button>
   );
 }
